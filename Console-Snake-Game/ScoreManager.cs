11e04f910ba4e62c277d9222e9bc66bb4dfaf7f0
Jon-Dev21 <jonacevedev96@gmail.com>
2021-12-04 23:56:31 -0400

    Created ScoreManager class used to manage game high scores

---
+++ Console-Snake-Game/ScoreManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Snake_Game
+{
+    /// <summary>
+    /// This class is used to access the game score. (SAVE, LOAD)
+    /// Score is held in a .csv file. Each record contains a player, 
+    /// it's symbol and high score.
+    /// </summary>
+    class ScoreManager
+    {
+    }
+}
