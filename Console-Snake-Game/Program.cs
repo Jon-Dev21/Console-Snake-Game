46c5d3ea3d348dcca550bc5104f0f56e125fbb92
Jon-Dev21 <jonacevedev96@gmail.com>
2021-12-04 23:41:45 -0400

    Program class automatically created. Used for testing.

---
+++ Console-Snake-Game/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Timers;
+
+namespace Console_Snake_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SnakeBoard board = new SnakeBoard(12, 12);
+
+            board.CreateBoard();
+
+            Console.WriteLine(board.GetBoardString());
+        }
+    }
+}
