452f032a0804f7a6e902e071685a2511a1f1313b
Jon-Dev21 <jonacevedev96@gmail.com>
2021-12-04 23:55:11 -0400

    Class used to store a snake player and it's properties

---
+++ Console-Snake-Game/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Snake_Game
+{
+    class Player
+    {
+        private string playerName;
+        private string playerSymbol;
+        private int playerHighScore;
+    }
+}
