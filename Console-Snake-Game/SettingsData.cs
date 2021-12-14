9d226ae8bf340cc54894374498660ac360f3000d
Jon-Dev21 <jonacevedev96@gmail.com>
2021-12-13 21:01:09 -0400

    Created new class SettingsData as a model for the game's settings

---
+++ Console-Snake-Game/SettingsData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Snake_Game
+{
+    /// <summary>
+    /// Class used to model the .csv data that is being used as the game settings.
+    /// </summary>
+    class SettingsData
+    {
+        public string ConsoleBackgroundColor { get; set; }
+        public string ConsoleTextColor { get; set; }
+        public int FontSize { get; set; }
+        public string Difficulty { get; set; }
+        public string Language { get; set; } // English and spanish for now.
+    }
+}
