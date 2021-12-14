3e897e613811c8e06f694afacf49cf1c484b2b2e
Jon-Dev21 <jonacevedev96@gmail.com>
2021-12-13 21:01:28 -0400

    Created score data class as a model for the game score

---
+++ Console-Snake-Game/ScoreData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Snake_Game
+{
+    /// <summary>
+    /// Class used to model the score data stored in the .csv file.
+    /// </summary>
+    class ScoreData
+    {
+        public string PlayerName { get; set; }
+        public char PlayerChar { get; set; }
+        public int Score { get; set; }
+    }
+}
