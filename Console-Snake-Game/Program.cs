c4ac8d9fa3d557b8260830e18eb6bd93f7170c1d
Jon-Dev21 <jonacevedev96@gmail.com>
2021-12-13 21:07:14 -0400

    Commenting code in and out and perforrming tests in current code. Still figuring out how to use timers and how to create functionality similar to javascript's setTimeout.

--- Console-Snake-Game/Program.cs
+++ Console-Snake-Game/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Timers;
 
 namespace Console_Snake_Game
@@ -7,11 +8,35 @@ namespace Console_Snake_Game
     {
         static void Main(string[] args)
         {
-            SnakeBoard board = new SnakeBoard(12, 12);
+            Board board = new Board(20, 40);
+            Player p = new Player("John", 'S', 5, 5);
+            //GameManager SnakeGame = new GameManager(board, p);
 
+            //board.MovePlayer();
             board.CreateBoard();
+            
 
+            board.AddPlayerToBoard(p);
+            board.SaveBoard();
             Console.WriteLine(board.GetBoardString());
+            //if (Console.ReadKey().Key == ConsoleKey.A)
+            //{
+            //    Console.WriteLine("Key pressed is A");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("I don't know what the kell you pressed.");
+            //}
+            //SnakeGame.StartGame();
+            //Console.WriteLine();
+
+
+            // Testing ScoreManager
+
+            //ScoreManager scoreManager = new ScoreManager(@"scores.csv");
+            //p.playerHighScore = 10;
+            //scoreManager.SetHighScore(p);
+            //scoreManager.WriteRecord();
         }
     }
 }
