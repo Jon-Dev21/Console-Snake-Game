f02b4ab1046d81438e8a6d1007059101af89db16
Jon-Dev21 <jonacevedev96@gmail.com>
2021-12-13 21:00:42 -0400

    Added code to store highscore in the specified url

--- Console-Snake-Game/ScoreManager.cs
+++ Console-Snake-Game/ScoreManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Console_Snake_Game
 {
@@ -13,5 +14,38 @@ namespace Console_Snake_Game
     /// </summary>
     class ScoreManager
     {
+        string URL;
+        private ScoreData scoreData;
+        /// <summary>
+        /// Array of records
+        /// </summary>
+        public string[] scoreDataRecord;
+
+        public ScoreManager(string url)
+        {
+            URL = url;
+            scoreData = new ScoreData();
+            scoreDataRecord = new string[1];
+        }
+
+        /// <summary>
+        /// Writes the score record into the score csv file.
+        /// </summary>
+        public void WriteRecord()
+        {
+            File.AppendAllLines(URL, scoreDataRecord);
+        }
+
+        /// <summary>
+        /// Sets the record of the passed player.
+        /// </summary>
+        /// <param name="p"></param>
+        public void SetHighScore(Player p)
+        {
+            scoreData.PlayerName = p.playerName;
+            scoreData.PlayerChar = p.playerBody[0].playerSymbol;
+            scoreData.Score = p.playerHighScore;
+            scoreDataRecord[0] = $"{scoreData.PlayerName}, {scoreData.PlayerChar}, {scoreData.Score}";
+        }
     }
 }
