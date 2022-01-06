c59dd00f5b92ed07a7e7bf2e60a59bb069db1949
Jon-Dev21 <jonacevedev96@gmail.com>
2022-01-06 01:26:21 -0400

    Multiple tests done. Working code being added in Game Manager
    Successfully tested functions in order to slowly build the program flow with the GameManager class.

--- Console-Snake-Game/Program.cs
+++ Console-Snake-Game/Program.cs
@@ -1,34 +1,93 @@
 ﻿using System;
 using System.IO;
 using System.Timers;
+using System.Threading;
+
+/*
+TO-DO LIST:
+ 
+- Create a game over method (Done)
+- Figure out how to move snake instead of printing the board all the time. (Thread to move snake?)
+- Create method to create a snake body and make it follow each other.
+- Add code to avoid going left while going right. (Done).
+- Create main menu and all windows and features. (Game manager should have all game windows)
+- Create fruit methods 
+- Create methods to load settings from a json text file. 
+  Settings such as score, keep track of how many wins/ losses / ratio, etc.
+
+1/5/2022
+
+Jael Opinions
+- Posible cambiar grid que sea en vez de size del array a size de la ventana.
+- Opciones para moverse con arrows
+ */
+
 
 namespace Console_Snake_Game
 {
     class Program
     {
+        static Board board;
+        static Player p;
+
+        static string lastInput = "d";
         static void Main(string[] args)
         {
-            Board board = new Board(20, 40);
-            Player p = new Player("John", 'S', 5, 5);
-            //GameManager SnakeGame = new GameManager(board, p);
 
-            //board.MovePlayer();
-            board.CreateBoard();
+            board = new Board(20, 36);
+            p = new Player("John", 'S', 15, 5);
+            GameManager SnakeGame = new GameManager(board, p);
+            SnakeGame.ShowMainMenu();
             
 
-            board.AddPlayerToBoard(p);
-            board.SaveBoard();
-            Console.WriteLine(board.GetBoardString());
-            //if (Console.ReadKey().Key == ConsoleKey.A)
+            // Create the game board.
+            //board.CreateBoard();
+
+            //// Creating a new thread to get the controller input.
+            //Thread gameInput = new(() =>
             //{
-            //    Console.WriteLine("Key pressed is A");
-            //}
-            //else
+
+            //    while (true)
+            //    {
+            //        ConsoleKeyInfo ConsoleKey = Console.ReadKey();
+            //        //Console.Write("Reading Key: ");
+            //        if (board.IsWallHit())
+            //        {
+            //            break;
+            //        } else
+            //        {
+            //            lastInput = ConsoleKey.Key.ToString();
+            //        }  
+            //    }
+            //});
+
+            //gameInput.Start();
+
+            //// Creating fruit for the game.
+            //Fruit f = new Fruit("F", 5, 10);
+
+            //// Adding the fruit to the board.
+            //board.AddFruitToBoard(f);
+
+            //// Main Thread will print the board 
+            //while (true)
             //{
-            //    Console.WriteLine("I don't know what the kell you pressed.");
+            //    ShowBoard();
+
+            //    Thread.Sleep(10);
+
+            //    // Check game over
+            //    if (board.IsWallHit())
+            //    {
+            //        break;
+            //    }
+            //    if (board.AteFruit(p, f))
+            //    {
+            //        board.AddFruitToBoard(f);
+            //    }
             //}
-            //SnakeGame.StartGame();
-            //Console.WriteLine();
+
+            //Console.WriteLine("Game Over.");
 
 
             // Testing ScoreManager
@@ -38,5 +97,22 @@ namespace Console_Snake_Game
             //scoreManager.SetHighScore(p);
             //scoreManager.WriteRecord();
         }
+
+        /// <summary>
+        /// Rename this function later to "PlayerMove"
+        /// </summary>
+        static void ShowBoard()
+        {
+            board.AddPlayerToBoard(p);
+            
+            Console.Clear();
+            p.Move(lastInput);
+
+
+            board.PrintBoard();
+            
+            //Console.WriteLine("Last Input: "+ lastInput);
+        }
+
     }
 }
