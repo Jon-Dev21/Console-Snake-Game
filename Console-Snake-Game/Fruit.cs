ae02251e3a6ceb39664bd803a3535d51ec707307
Jon-Dev21 <jonacevedev96@gmail.com>
2022-01-06 01:18:12 -0400

    Added new Fruit class to represent a fruit that the snake would eat. This Fruit spawns in the board once eaten

---
+++ Console-Snake-Game/Fruit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Snake_Game
+{
+    class Fruit
+    {
+        public int FruitX { get; set; }
+        public int FruitY { get; set; }
+        public int OldFruitX { get; set; }
+        public int OldFruitY { get; set; }
+        public string FruitSymbol { get; set; }
+
+        public Fruit(string fruitSymbol, int fruitX, int fruitY)
+        {
+            FruitSymbol = fruitSymbol;
+            FruitX = fruitX;
+            FruitY = fruitY;
+        }
+    }
+}
